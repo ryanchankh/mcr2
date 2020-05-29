d9e58121a4ecc9728b44edae56ac389b862f9b90
Yu YaoDong <yaodong@lymphoma.cs.ucdavis.edu>
2020-05-29 01:43:03 -0700

    add loss 6

--- train_selfsup.py
+++ train_selfsup.py
@@ -52,16 +52,14 @@ parser.add_argument('--pretrain_epo', type=int, default=None,
                     help='load pretrained epoch for assigning labels')
 parser.add_argument('--savedir', type=str, default='/mnt/raid/user/yaodong/saved_models/',
                     help='base directory for saving PyTorch model. (default: ./saved_models/)')
-
-parser.add_argument('--loss', type=int)
 args = parser.parse_args()
 
 
 ## Pipelines Setup
 model_dir = os.path.join(args.savedir,
-               'selfsup_{}+{}_{}_epo{}_bs{}+{}_aug{}+{}_lr{}_mom{}_wd{}_gam1{}_gam2{}_gam3{}_eps{}_loss{}{}'.format(
+               'selfsup_{}+{}_{}_epo{}_bs{}+{}_aug{}+{}_lr{}_mom{}_wd{}_gam1{}_gam2{}_gam3{}_eps{}{}'.format(
                     args.arch, args.fd, args.data, args.epo, args.bs, args.sampler, args.aug, args.transform, args.lr, 
-                    args.mom, args.wd, args.gam1, args.gam2, args.gam3, args.eps, args.loss, args.tail))
+                    args.mom, args.wd, args.gam1, args.gam2, args.gam3, args.eps, args.tail))
 headers = ["epoch", "step", "loss", "discrimn_loss_e", "compress_loss_e", 
             "discrimn_loss_t",  "compress_loss_t", "compress_loss_ortho"]
 utils.init_pipeline(model_dir, headers=headers)
@@ -77,14 +75,6 @@ def lr_schedule(epoch, optimizer):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def plabels_schedule(epoch, net, data, trainloader, n_clusters=10, gamma=100):
-    """update labels using clutsering."""
-    if epoch % 5 == 0: #test
-        plabels, accuracy = tf.get_plabels(net, data, n_clusters=n_clusters, gamma=100)
-        trainloader.update_labels(plabels)
-        utils.save_labels(model_dir, plabels, epoch)
-        print(f'epoch {epoch} - accuracy {accuracy}')
-
 
 ## Prepare for Training
 if args.pretrain_dir is not None:
@@ -98,18 +88,15 @@ trainloader = AugmentLoader(trainset,
                             sampler=args.sampler,
                             batch_size=args.bs,
                             num_aug=args.aug)
-if args.loss == 2:
-    criterion = CompressibleLoss2(gam1=args.gam1, gam2=args.gam2, gam3=args.gam3, eps=args.eps, num_aug=args.aug)
-elif args.loss == 4:
-    criterion = CompressibleLoss4(gam1=args.gam1, gam2=args.gam2, gam3=args.gam3, eps=args.eps, num_aug=args.aug)
+
+criterion = CompressibleLoss6(gam1=args.gam1, gam2=args.gam2, gam3=args.gam3, eps=args.eps, num_aug=args.aug)
 optimizer = SGD(net.parameters(), lr=args.lr, momentum=args.mom, weight_decay=args.wd)
 
 
 ## Training
 for epoch in range(args.epo):
     # lr_schedule(epoch, optimizer)
-    # plabels_schedule(epoch, net, args.data, trainloader, n_clusters=args.nc)
-    for step, (batch_imgs, batch_lbls, batch_idx) in enumerate(trainloader):
+    for step, (batch_imgs, _, batch_idx) in enumerate(trainloader):
         batch_features = net(batch_imgs.cuda())
         loss, loss_empi, loss_theo, loss_ortho = criterion(batch_features, batch_idx)
 
