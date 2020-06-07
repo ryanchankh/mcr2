455bc4abad70fc88cd6c7abd1dc7a3747d3c070b
Ryan Chan <ryanchankh@berkeley.edu>
2020-06-06 19:10:55 -0700

    remove references

--- ce_training/models/resnet.py
+++ ce_training/models/resnet.py
@@ -1,11 +1,3 @@
-'''ResNet in PyTorch.
-
-For Pre-activation ResNet, see 'preact_resnet.py'.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
