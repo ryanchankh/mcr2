f2c65cc8f902d3bb92b68c2976f235f3f5f8ae9a
ryanchankh <ryanchankh@berkeley.edu>
2020-06-27 03:10:34 -0700

    update evaluate

--- evaluate.py
+++ evaluate.py
@@ -128,6 +128,8 @@ if __name__ == '__main__':
     trainset = tf.load_trainset(params['data'], train_transforms, train=True, path=args.data_dir)
     if 'lcr' in params.keys(): # supervised corruption case
         trainset = tf.corrupt_labels(trainset, params['lcr'], params['lcs'])
+    if 'class_to_keep' in params.keys():
+        trainset = tf.filter_class(trainset, params['class_to_keep'])
     new_labels = trainset.targets
     trainloader = DataLoader(trainset, batch_size=200)
     train_features, train_labels = tf.get_features(net, trainloader)
