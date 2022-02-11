2bcdff7074fd5d1cc8f555fa1c7f3c3a192400fe
Tianjiao Ding <11525570+tianjiaoding@users.noreply.github.com>
2022-02-11 18:12:12 -0500

    Update train_func.py
    added normalization for cifar

--- train_func.py
+++ train_func.py
@@ -139,6 +139,15 @@ def load_transforms(name):
             transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
             transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor()])
+    elif _name == "cifar_norm":
+        transform = transforms.Compose([
+            transforms.RandomResizedCrop(32),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
     elif _name == "mnist":
          transform = transforms.Compose([
             transforms.RandomChoice([
